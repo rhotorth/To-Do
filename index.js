d3f7c13a663306a373d90dd579bfa47dedc62793
Max Teleshko <makakattttttt@gmail.com>
2025-07-01 20:03:05 +0300

    Basic version of this app

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /*----------------
 *       TODO
 *
-*   Build html structure
-*   Style the whole thing
-*   Dark mode switch
 *   Module spread
 *
 *----------------
@@ -19,7 +16,6 @@ const field = document.getElementById('objective-field');
 const darkModeButton = document.getElementById('darkmode-switch');
 const addObjectiveButton = document.getElementById('objective-button');
 const objectiveContainer = document.getElementById('objectives-container');
-const checkboxes = document.querySelectorAll('accomplished');
 
 function createObjective () {
     const currentBackgroundColor = getComputedStyle(body).backgroundColor;
@@ -45,30 +41,43 @@ function createObjective () {
 
         objective.innerHTML = `
         <p class='objective-para'>${newObjective.objective}</p>
-        <input class='accomplished' type='checkbox' data-index=${index}>`;
+        <input class='accomplished' type='checkbox' data-index=${index}>
+        <button class='delete-button'>Delete</button>`;
 
         const checkbox = objective.querySelector('.accomplished');
-
         checkbox.addEventListener('change', accomplish);
 
-        if (currentBackgroundColor === 'rgb(240, 240, 240)') {
-
-        } else {
-
-        }
 
         objectiveContainer.style.opacity = 100;
         objectiveContainer.appendChild(objective);
+
+        if (currentBackgroundColor === 'rgb(240, 240, 240)') {
+            objective.style.backgroundColor = '#f0f0f0';
+        } else {
+            objective.style.backgroundColor = '#27272a';
+            objective.style.color = 'white';
+        }
+
     }
 }
 
 function accomplish (event) {
     const index = event.target.dataset.index;
+    const objectiveDiv = event.target.closest('.objective');
+    const paragraphs = objectiveDiv.querySelector('.objective-para');
+    
     objectives[index].isAccomplished = event.target.checked;
+
+    if (objectives[index].isAccomplished) {
+        paragraphs.classList.add('done');
+    } else {
+        paragraphs.classList.remove('done');
+    }
 }
 
 function switchMode () {
     const currentBackgroundColor = getComputedStyle(body).backgroundColor;
+    const objectiveDiv = document.querySelectorAll('.objective');
 
     if (currentBackgroundColor === 'rgb(240, 240, 240)') {
         body.style.backgroundColor = '#27272a';
@@ -83,11 +92,16 @@ function switchMode () {
         objectiveContainer.style.backgroundColor = '#18181b';
         errorParagraph.style.color = 'white';
 
+        for (let i = 0; i < objectiveDiv.length; i++) {
+            objectiveDiv[i].style.backgroundColor = '#27272a';
+            objectiveDiv[i].style.color = 'white';
+        }
+
         darkModeButton.textContent = 'Switch to Light Mode';
     } else {
         body.style.backgroundColor = '#f0f0f0';
         container.style.backgroundColor = 'white';
-        h1.style.color = 'black';
+        h1.style.color = '#27272a';
         field.style.backgroundColor = '#f0f0f0';
         field.style.color = 'black';
         darkModeButton.style.backgroundColor = '#e7e5e4';
@@ -96,6 +110,11 @@ function switchMode () {
         addObjectiveButton.style.color = 'black';
         objectiveContainer.style.backgroundColor = 'white';
         errorParagraph.style.color = 'black';
+
+        for (let i = 0; i < objectiveDiv.length; i++) {
+            objectiveDiv[i].style.backgroundColor = '#f0f0f0';
+            objectiveDiv[i].style.color = 'black';
+        }
 
         darkModeButton.textContent = 'Switch to Dark Mode';
     }
